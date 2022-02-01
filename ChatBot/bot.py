a808f549b7fe95ed5ed984faf9f46b4e25b3f6ba
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 15:28:19 +0300

    ChatBot 2-nd stage

--- ChatBot/bot.py
+++ ChatBot/bot.py
@@ -1,2 +1,5 @@
 print('Hello! My name is 口ひげを生やした帽子 .')
 print('I was created in 2022')
+print('Please, remind me your name.')
+your_name = input("name:")
+print('What a great name you have,' + your_name + '!')
