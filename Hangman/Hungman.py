e825814c0b18a54de7e89ebe48b98ba7363abf68
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 15:41:57 +0300

    Hamngman 3-rd stage

--- Hangman/Hungman.py
+++ Hangman/Hungman.py
@@ -6,3 +6,7 @@ try_words = []
 vse_bukvi = []
 life = 8
 words = ['python', 'java', 'kotlin', 'javascript']
+random_word = random.choice(words)
+b = '-' * (len(random_word))
+random_word = list(random_word)
+random_word_set = set(random_word)
