dcd58c70b38cbbec674606fb799faf5717e8d564
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 16:18:28 +0300

    TikTakToe 1-st stage

---
+++ TikTakToe/TikTakToe.py
@@ -0,0 +1,10 @@
+import random
+import sys
+
+AI_TURN = True
+USER_TURN = False
+
+win_lines = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+             (0, 4, 8), (2, 4, 6), (0, 3, 6),
+             (1, 4, 7), (2, 5, 8)]
+
