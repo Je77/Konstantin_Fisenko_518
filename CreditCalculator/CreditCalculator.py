66a3b5c3b8e52a6d0cc68f15eb9172c7fd552c69
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 21:39:20 +0300

    CreditCalculator 2-nd stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -2,3 +2,13 @@ from math import pow,ceil,log,floor
 import argparse
 
 
+parser = argparse.ArgumentParser(description="Credit Calculator Project")
+parser.add_argument('--type', help="Type of Payment (Annuity or Differential")
+parser.add_argument('--payment', help="Monthly payment", type=int)
+parser.add_argument('--principal', help="Credit principal", type=int)
+parser.add_argument('--periods', help="Count of months", type=int)
+parser.add_argument('--interest', help="Credit interest (rate of interest)", type=float)
+args = parser.parse_args()
+
+
+
