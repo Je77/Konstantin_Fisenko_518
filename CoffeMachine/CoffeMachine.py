2b58a42896389a10c9c0d056cf140868576c4de8
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 16:02:28 +0300

    CoffeeMachine 3-rd stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -16,4 +16,17 @@ def print_has():
     print(f"${CoffeeMachine.money} of money")
 
 
+while True:
+    print(" ")
+    water_can_espresso = CoffeeMachine.water // 250
+    coffee_can_espresso = CoffeeMachine.coffee // 16
+
+    water_can_latte = CoffeeMachine.water // 350
+    coffee_can_latte = CoffeeMachine.coffee // 20
+    milk_can_latte = CoffeeMachine.milk // 75
+
+    water_can_cappuccino = CoffeeMachine.water // 200
+    coffee_can_cappuccino = CoffeeMachine.coffee // 12
+    milk_can_cappuccino = CoffeeMachine.milk // 100
+
 
