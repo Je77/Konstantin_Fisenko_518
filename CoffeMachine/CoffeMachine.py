74012250c71c4e55a3ae3158ba4901bef5a595ea
Je77 <k.v.fisenko@student.khai.edu>
2022-02-01 16:01:36 +0300

    CoffeeMachine 2-nd stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -5,3 +5,15 @@ class CoffeeMachine:
     cups = 9
     money = 550
 
+
+def print_has():
+    print(" ")
+    print("The coffee machine has:")
+    print(f"{CoffeeMachine.water} of water")
+    print(f"{CoffeeMachine.milk} of milk")
+    print(f"{CoffeeMachine.coffee} of coffee beans")
+    print(f"{CoffeeMachine.cups} of disposable cups")
+    print(f"${CoffeeMachine.money} of money")
+
+
+
