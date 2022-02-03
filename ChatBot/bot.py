8f77d9d30ac305d519febc2a33f913f6b2156fc8
Je77 <k.v.fisenko@student.khai.edu>
2022-02-03 19:47:58 +0300

    ChatBot 3-rd stage

--- ChatBot/bot.py
+++ ChatBot/bot.py
@@ -2,3 +2,13 @@ print('Hello! My name is 口ひげを生やした帽子 .')
 print('I was created in 2022')
 print('Please, remind me your name.')
 your_name = input("name:")
+print('What a great name you have,' + your_name + '!')
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = (float(input("на 3:")))
+remainder5 = (float(input("на 5:")))
+remainder7 = (float(input("на 7:")))
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+number = int(input())
+number += 1
