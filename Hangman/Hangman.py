c95dc48e8986458192968db2ba25b52773d454ba
Je77 <k.v.fisenko@student.khai.edu>
2022-02-04 16:13:47 +0300

    Hangman 2-nd stage

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -4,5 +4,10 @@ print("""H A N G M A N
 The game will be available soon.""")
 try_words = []
 vse_bukvi = []
-
+life = 8
+words = ['python', 'java', 'kotlin', 'javascript']
+random_word = random.choice(words)
+b = '-' * (len(random_word))
+random_word = list(random_word)
+random_word_set = set(random_word)
 
