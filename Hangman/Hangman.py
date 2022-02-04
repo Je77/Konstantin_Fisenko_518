836c8f15cbd4aed73dfcbfb580c9f7ba6a69f3f5
Je77 <k.v.fisenko@student.khai.edu>
2022-02-04 16:15:41 +0300

    Hangman 6-th stage

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -30,5 +30,21 @@ while True:
                 print('No improvements')
                 life -= 1
                 print(life)
+            elif letter in random_word:
+                print(life)
 
+            elif letter != random_word:
+                if letter in vse_bukvi and letter not in try_words:
+                    print("You've already guessed this letter.")
+                else:
+                    life -= 1
+                    print("That letter does not appear in the word")
+                    print(life)
+            vse_bukvi.append(letter)
+            for i in random_word:
+                if letter == i:
+                    try_words.append(letter)
+            for letter in random_word:
+                if letter in try_words:
+                    print(letter, end='')
 
